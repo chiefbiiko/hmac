76e2b265fd9f2632f61b71e4864151fb693f8df3
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-06-07 20:21:16 +0200

    prettier log in usage example

--- usage.ts
+++ usage.ts
@@ -6,4 +6,4 @@ const msg: string = "money";
 // the simple way
 const mac: Uint8Array = hmac("sha256", key, msg);
 
-console.log(`key ${key}, msg ${msg} -> mac ${mac}`);
+console.log(`key ${key}\nmsg ${msg}\nmac ${mac}`);
